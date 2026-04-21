e0254d6778de0115164a5b104d19469b03c875b7
ec25779 <ec25779@qmul.ac.uk>
2026-04-21 16:46:12 +0100

    chore: setup JUnit testing framework

---
+++ src/test/java/com/github/ec25779/digitalid/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.github.ec25779.digitalid;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testExample() {
+        assertEquals(1 + 2, 3);
+    }
+
+}
